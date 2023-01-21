88021e7603713f7e622054e70a57ad1187864b79
Clayton Rafael <clayton.rssouza@gmail.com>
2023-01-21 10:00:51 -0300

    feat: implement form to add tasks

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,15 +1,15 @@
-import styles from "./Form.module.css";
+import styles from './Form.module.css'
 
 const Form = () => {
   return (
     <div className={styles.form}>
-      <input type="text" />
+      <input type="text" placeholder="Adicione uma nova tarefa" />
       <button>
-        <span>Criar</span>{" "}
-        <img src="./plus-icon.svg" alt="Ícone de adicionar" />{" "}
+        <span>Criar</span>{' '}
+        <img src="./plus-icon.svg" alt="Ícone de adicionar" />{' '}
       </button>
     </div>
-  );
-};
+  )
+}
 
-export default Form;
+export default Form
